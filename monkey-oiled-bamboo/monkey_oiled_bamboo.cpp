234cf9d539bb208b6c36c443c78b1eeabf16c606
Luca Ceccagnoli <{ID}+{username}@users.noreply.github.com>
2022-01-07 11:35:14 +0100

    monkey and oiled bamboo

--- monkey-oiled-bamboo/monkey_oiled_bamboo.cpp
+++ monkey-oiled-bamboo/monkey_oiled_bamboo.cpp
@@ -1,13 +1,18 @@
-#include <iostream>;
-#include <vector>;
+#include <iostream>
+#include <vector>
 
 int calc_min_jump_strength(std::vector<int> &tree){
     int min_strength = 0;
     for(int i=tree.size() - 1; i > 0; i--)
     {
-        if(tree[i] - tree[i-1] > min_strength)
+        if(min_strength < tree[i] - tree[i-1])
         {
             min_strength = tree[i] - tree[i-1];
+        }
+        else if(min_strength == tree[i] - tree[i-1])
+        {
+            // found the same height twice: need to add 1 to min strength
+            min_strength++;
         }
     }
     return min_strength;
